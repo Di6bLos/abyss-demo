593ea5479d5015718b55c056682d9500f0cc65a2
CarlosPrieto <carlosprieto66@icloud.com>
2025-11-12 11:06:53 -0700

    Add GitHub Actions workflow for deploying to GitHub Pages and update navigation links for correct routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    server: {
-      fs: {
-        // Allow serving files from one level up to the project root
-        allow: ['..']
-      }
-    }
-  }
+	site: 'https://Di6bLos.github.io',
+	base: '/abyss-demo/',
+	vite: {
+		server: {
+			fs: {
+			// Allow serving files from one level up to the project root
+			allow: ['..']
+			}
+		}
+	}
 });
