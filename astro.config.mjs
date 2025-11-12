87f8cb2495f8712cb4d84d998d4f079a54e0aa56
CarlosPrieto <carlosprieto66@icloud.com>
2025-11-12 13:39:14 -0700

    Fix site URL casing and update image source path in index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://Di6bLos.github.io',
+	site: 'https://di6blos.github.io',
 	base: '/abyss-demo/',
+	output: 'static',
 	vite: {
 		server: {
 			fs: {
